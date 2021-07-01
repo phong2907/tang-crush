a3ed7a4000abba98570869dd4b8ecb9fb505ee0e
phong2907 <86787782+phong2907@users.noreply.github.com>
2021-07-01 21:55:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     title: 'trên thế giới có hơn 7 tỉ nụ cười nhưng đối với tôi nụ cười ấy vẫn là đẹp nhất,thế nên là làm ơn!!',
     desc: 'hãy để tôi giữ nụ cười ấy trên môi được không?  CHẤP NHẬN ĐI =)))',
     btnYes: 'okkk luônnnnn <33',
-    btnNo: 'Không được :3',
+    btnNo: 'Không được. :3',
     question:'Trên thế giới hơn 7 tỉ người mà yêu đúng 1 người <3',
     btnReply: 'Gửi cho mình <3',
     reply: 'Yêu luônn <3',
