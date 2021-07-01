dc25f9134e1e14b9b6378ea8028c528831e601d2
phong2907 <86787782+phong2907@users.noreply.github.com>
2021-07-01 21:52:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Quìn eiiiii!',
+    introTitle: 'Bạn gì eiiiii!',
     introDesc: `bấm vào đây này
     có cái này hay lắm -)))`,
     btnIntro: 'okkkk',
-    title: 'trên thế giới có hơn 7 tỉ nụ cười nhưng đối với t nụ cười ấy vẫn là đẹp nhất,thế nên là làm ơn!!',
-    desc: 'hãy để t giữ nụ cười ấy trên môi được không?  không chơi thoát ra nháaa',
-    btnYes: 'okkk được luônnnnn <33',
+    title: 'trên thế giới có hơn 7 tỉ nụ cười nhưng đối với tôi nụ cười ấy vẫn là đẹp nhất,thế nên là làm ơn!!',
+    desc: 'hãy để tôi giữ nụ cười ấy trên môi được không?  CHẤP NHẬN ĐI =)))',
+    btnYes: 'okkk luônnnnn <33',
     btnNo: 'Không được :3',
-    question:'Trên thế giới hơn 7 tỉ người mà yêu đúng 1 ngươi <3',
-    btnReply: 'Gửi cho t <3',
+    question:'Trên thế giới hơn 7 tỉ người mà yêu đúng 1 người <3',
+    btnReply: 'Gửi cho mình <3',
     reply: 'Yêu luônn <3',
     mess: 'không yêu cũng phải yêu 😘😘',
-    messDesc: 'thi xong yeu nhau khong? -)).',
+    messDesc: ' yeu nhau khong? -)).',
     btnAccept: 'Okiiiii luônn. <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
